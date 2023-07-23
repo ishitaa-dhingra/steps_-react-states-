8ca8bfe63d840e5f9f21549e7103a9caf31da61c
Ishitaa Dhingra <ishitaadhingra@gmail.com>
2023-07-24 04:22:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+export default function App() {
+  const [step, setstep] = useState(1);
+  const [isopen, setisopen] = useState(true);
+
+  function handlePrevious() {
+    if (step > 1) setstep((step) => step - 1);
+    // ------------------------------------------ this version is better than below
+  }
+
+  function handleNext() {
+    if (step < 3) setstep(step + 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // -------------------------we dont want the components inside some other component thus we used fragmentation
+    <>
+      <div>
+        <button className="close" onClick={() => setisopen((is) => !is)}>
+          &times;
+        </button>
+      </div>
+
+      {isopen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={`${step >= 1 ? "active " : ""}`}>1</div>
+            <div className={`${step >= 2 ? "active " : ""}`}>2</div>
+            <div className={`${step >= 3 ? "active " : ""}`}>3</div>
+          </div>
+          <p className="message">
+            Step {step}:{messages[step - 1]}
+          </p>
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handlePrevious}
+            >
+              {/* ---------------------------------------------here we dont call function (handlePrevious())because we have to call fuction when clicked */}
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
-
-export default App;
